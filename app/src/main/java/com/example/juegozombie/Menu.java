d8f62a87abd97d61b0e2d9e898a191a2d837f1e2
Ronny <ronnychamba96@gmail.com>
2022-07-15 23:00:50 -0500

    Funcion cerrar sesion

--- app/src/main/java/com/example/juegozombie/Menu.java
+++ app/src/main/java/com/example/juegozombie/Menu.java
@@ -4,16 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity  implements View.OnClickListener {
 
 
     private FirebaseAuth auth;
     private FirebaseUser user;
+    private Button btnCerrarSesion;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,19 +33,46 @@ public class Menu extends AppCompatActivity {
         super.onStart();
     }
 
-    private void initApp(){
-
+    private void initApp() {
+        findByWidget();
+        setListenerClick();
         auth = FirebaseAuth.getInstance();
         user = auth.getCurrentUser();
     }
-    private void usuarioLogeado(){
 
-        if (user !=null){
+
+    private void findByWidget() {
+        btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+    }
+
+    private void setListenerClick() {
+        btnCerrarSesion.setOnClickListener(this);
+    }
+
+    private void usuarioLogeado() {
+
+        if (user != null) {
             Toast.makeText(this, "en linea", Toast.LENGTH_LONG).show();
-        }else{
-            startActivity( new Intent(this, MainActivity.class));
+        } else {
+            startActivity(new Intent(this, MainActivity.class));
             finish();
         }
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+
+        if (id == btnCerrarSesion.getId()){
+            cerrarSesion();
+        }
+    }
+    private void cerrarSesion(){
+        auth.signOut();
+        startActivity(new Intent(Menu.this, MainActivity.class));
+        Toast.makeText(this, "Sesión cerrada exitosamente", Toast.LENGTH_SHORT).show();
 
     }
 }
